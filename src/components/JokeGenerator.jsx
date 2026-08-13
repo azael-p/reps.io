1b6edaa92b9a6b7bcd9b88a660a6bf557c195564
azael-p <pignanessiazael@gmail.com>
2026-08-13 20:48:27 -0300

    chore: remove JokeGenerator component per owner request

--- src/components/JokeGenerator.jsx
+++
@@ -1,4 +0,0 @@
-// REMOVED: JokeGenerator component deleted per repository owner request
-// Original commit: cc50afb124cc803ef15ab71703a08e762084ae22
-// If you need a full revert of the commit history, run locally:
-//   git revert cc50afb124cc803ef15ab71703a08e762084ae22
